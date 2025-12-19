692752bbe5be312fb7e1ae88517440c1968e8b2d
ajrilajar <100751598+ajrilajar@users.noreply.github.com>
2025-12-19 17:43:17 +0700

    asdas

--- app.js
+++ app.js
@@ -9,35 +9,28 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
-const renderZone = document.getElementById('inventory-render');
+const container = document.getElementById('inventory-list');
 
 onValue(ref(db, 'products'), (snapshot) => {
     const data = snapshot.val();
-    renderZone.innerHTML = "";
+    container.innerHTML = "";
     
-    for (let key in data) {
-        const p = data[key];
-        const html = `
-            <div class="item-card">
-                <div class="box-initial">${p.nama.substring(0,2).toUpperCase()}</div>
-                <div class="info-center">
-                    <div class="name-product">${p.nama}</div>
-                    <div class="price-container">
-                        <div>
-                            <div class="price-label">Penjualan</div>
-                            <div class="price-val">Rp ${Number(p.hargaJual).toLocaleString('id-ID')}</div>
-                        </div>
-                        <div>
-                            <div class="price-label">Pembelian</div>
-                            <div class="price-val">Rp ${Number(p.hargaBeli).toLocaleString('id-ID')}</div>
-                        </div>
+    for (let id in data) {
+        const item = data[id];
+        container.innerHTML += `
+            <div class="card-item">
+                <div class="col-initial">${item.nama.substring(0,2).toUpperCase()}</div>
+                <div class="col-info">
+                    <div class="product-name">${item.nama}</div>
+                    <div class="price-row">
+                        <div><div class="price-label">Penjualan</div><div class="price-value">Rp ${Number(item.hargaJual).toLocaleString('id-ID')}</div></div>
+                        <div><div class="price-label">Pembelian</div><div class="price-value">Rp ${Number(item.hargaBeli).toLocaleString('id-ID')}</div></div>
                     </div>
                 </div>
-                <div class="info-right">
-                    <span class="badge-cat">${p.kategori}</span>
-                    <div class="total-stock">${p.stok} ${p.satuan || 'PCS'}</div>
+                <div class="col-stock">
+                    <span class="tag-cat">${item.kategori}</span>
+                    <div class="stock-text">${item.stok} ${item.satuan || 'PCS'}</div>
                 </div>
             </div>`;
-        renderZone.innerHTML += html;
     }
 });
